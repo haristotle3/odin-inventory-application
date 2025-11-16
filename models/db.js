819e6d7907d2d7b3db84c0168ab251ec43468c91
haristotle <haristotlepatil2003@gmail.com>
2025-11-16 20:36:08 +0530

    Implement pokemon and trainer READ operations search, filter, getTypes for pokemon and getAllTrainerNameAndIDs for trainers

--- models/db.js
+++ models/db.js
@@ -18,6 +18,12 @@ class Database {
     return rows;
   }
 
+  async getAllTrainerNameAndIDs() {
+    const query = "SELECT id, name FROM trainers;";
+    const { rows } = await this.db.query(query);
+    return rows;
+  }
+
   async getTrainerByID(trainerID) {
     const query = "SELECT * FROM trainers WHERE id = $1;";
     const { rows } = await this.db.query(query, [trainerID]);
@@ -35,6 +41,26 @@ class Database {
     const { rows } = await this.db.query(query, [pokemonID]);
     return rows;
   }
+
+  async filterPokemon(type, trainerID) {
+    const query = "SELECT pokemon.* FROM pokemon JOIN trainers ON pokemon.trainer_id = trainers.id WHERE type = COALESCE($1, type) AND trainers.id = COALESCE($2, trainers.id);"
+    const { rows } = await this.db.query(query, [type, trainerID]);
+    return rows;
+  }
+
+  async searchPokemon(startPattern) {
+    const query = "SELECT * FROM pokemon WHERE name ILIKE $1;";
+    const { rows } = await this.db.query(query, [`${startPattern}%`]);
+    return rows;
+  }
+
+  async getAllPokemonTypes() {
+    const query = "SELECT DISTINCT type FROM pokemon;";
+    const { rows } = await this.db.query(query);
+    return rows;
+  }
+
+
 }
 
 const db = new Database();
