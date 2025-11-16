f31d96f4d31aa187856f4de4ef3e5390aeb7599f
haristotle <haristotlepatil2003@gmail.com>
2025-11-16 20:38:39 +0530

    Implement POST route handler for pokemon Use multer and implement disk storage functionality

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -1,9 +1,26 @@
 import { Router } from "express";
-import { pokemonDefaultController, pokemonIDController } from "../controllers/pokemonController.js";
+import {
+  addPokemonController,
+  pokemonDefaultController,
+  pokemonIDController,
+} from "../controllers/pokemonController.js";
+import multer from "multer";
 
+const pokemonStorage = multer.diskStorage({
+  destination: "public/images/pokemon",
+  
+  filename: function (req, file, cb) {
+    const { name } = req.body;
+    const extension = file.originalname.split(".").pop();
+    cb(null, name + "." + extension)
+  }
+})
+
+const pokemonUpload = multer({ storage: pokemonStorage })
 const pokemonRouter = Router();
 
-pokemonRouter.get("/", pokemonDefaultController)
-pokemonRouter.get("/:id", pokemonIDController)
+pokemonRouter.get("/", pokemonDefaultController);
+pokemonRouter.get("/:id", pokemonIDController);
+pokemonRouter.post("/", pokemonUpload.single("photo"), addPokemonController);
 
 export default pokemonRouter;
