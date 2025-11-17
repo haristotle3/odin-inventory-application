500f2f41d505802e6be9e4f76dc65d1806039b2a
haristotle <haristotlepatil2003@gmail.com>
2025-11-18 00:22:33 +0530

    Render trainers template instead of index template

--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -2,11 +2,11 @@ import db from "../models/db.js";
 
 export async function trainerDefaultController(req, res) {
   const rows = await db.getAllTrainers();
-  res.render("index", { trainers: rows });
+  res.render("trainers", { trainers: rows });
 }
 
 export async function trainerIDController(req, res) {
   const trainerID = req.params.id
   const row = await db.getTrainerByID(trainerID);
-  res.render("index", { trainers: row });
+  res.render("trainers", { trainers: row });
 }
