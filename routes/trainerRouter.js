223065291c1fd07d56284d8e8bfc47776676ce1b
haristotle <haristotlepatil2003@gmail.com>
2025-11-23 22:53:29 +0530

    Implement route handler for update trainer patch method

--- routes/trainerRouter.js
+++ routes/trainerRouter.js
@@ -4,6 +4,7 @@ import {
   trainerIDController,
   addTrainerController,
   deleteTrainer,
+  updateTrainer
 } from "../controllers/trainerController.js";
 import { verifyDestructiveAction } from "../controllers/passwordVerificationHelper.js";
 
@@ -21,11 +22,13 @@ const trainerStorage = multer.diskStorage({
 const trainerUpload = multer({ storage: trainerStorage });
 const trainerRouter = Router();
 
-trainerRouter.get("/", trainerDefaultController);
-trainerRouter.post("/", trainerUpload.single("trainerPhoto"), addTrainerController);
-
 trainerRouter.get("/:id", trainerIDController);
 trainerRouter.post("/:id", verifyDestructiveAction);
 trainerRouter.delete("/:id", deleteTrainer);
+trainerRouter.patch("/:id", updateTrainer);
+
+trainerRouter.get("/", trainerDefaultController);
+trainerRouter.post("/", trainerUpload.single("trainerPhoto"), addTrainerController);
+
 
 export default trainerRouter;
