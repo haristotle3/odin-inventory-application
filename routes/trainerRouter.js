45e313312dca95bd77f13e6587cd3fbdd41ff13f
haristotle <haristotlepatil2003@gmail.com>
2025-11-23 19:14:38 +0530

    Implement add trainer route Use multer for file storage

--- routes/trainerRouter.js
+++ routes/trainerRouter.js
@@ -1,9 +1,25 @@
 import { Router } from "express";
-import { trainerDefaultController, trainerIDController } from "../controllers/trainerController.js";
+import {
+  trainerDefaultController,
+  trainerIDController,
+  addTrainerController,
+} from "../controllers/trainerController.js";
+import multer from "multer";
 
+const trainerStorage = multer.diskStorage({
+  destination: "public/images/trainers",
+  filename: (req, file, cb) => {
+    const { newTrainerName } = req.body;
+    const extension = file.originalname.split(".").pop();
+    cb(null, newTrainerName + "." + extension);
+  },
+});
+
+const trainerUpload = multer({ storage: trainerStorage });
 const trainerRouter = Router();
 
 trainerRouter.get("/", trainerDefaultController);
+trainerRouter.post("/", trainerUpload.single("trainerPhoto"), addTrainerController);
 trainerRouter.get("/:id", trainerIDController);
 
 export default trainerRouter;
