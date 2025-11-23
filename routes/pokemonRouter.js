0efc4241f37955a37e38699b73a70607597740c1
haristotle <haristotlepatil2003@gmail.com>
2025-11-23 21:59:12 +0530

    Edit imports

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -3,23 +3,23 @@ import {
   pokemonIndexController,
   pokemonIDController,
   addPokemonController,
-  verifyDestructiveAction,
   deletePokemonController,
-  updatePokemonController
+  updatePokemonController,
 } from "../controllers/pokemonController.js";
+import { verifyDestructiveAction } from "../controllers/passwordVerificationHelper.js";
 import multer from "multer";
 
 const pokemonStorage = multer.diskStorage({
   destination: "public/images/pokemon",
-  
+
   filename: function (req, file, cb) {
     const { name } = req.body;
     const extension = file.originalname.split(".").pop();
-    cb(null, name + "." + extension)
-  }
-})
+    cb(null, name + "." + extension);
+  },
+});
 
-const pokemonUpload = multer({ storage: pokemonStorage })
+const pokemonUpload = multer({ storage: pokemonStorage });
 const pokemonRouter = Router();
 
 pokemonRouter.get("/:id", pokemonIDController);
