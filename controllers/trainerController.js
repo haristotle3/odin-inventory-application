27bf23e769ba07fd5a8dae27e5f105de45094f0e
haristotle <haristotlepatil2003@gmail.com>
2025-11-24 11:37:14 +0530

    Fix typo fileUrlToPath -> fileURLToPath

--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -4,34 +4,40 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export async function trainerDefaultController(req, res) {
-  const rows = await db.getAllTrainers();
+  let rows;
+
+  if (req.query.trainerName) {
+    rows = await db.searchTrainers(req.query.trainerName);
+  } else {
+    rows = await db.getAllTrainers();
+  }
   res.render("trainers", { trainers: rows });
 }
 
 export async function trainerIDController(req, res) {
-  const trainerID = req.params.id
+  const trainerID = req.params.id;
   const row = await db.getTrainerByID(trainerID);
-  const trainersPokemon = await db.getAllPokemonOfTrainer(trainerID)
+  const trainersPokemon = await db.getAllPokemonOfTrainer(trainerID);
   res.render("individualTrainers", { trainers: row, trainersPokemon });
 }
 
 export async function addTrainerController(req, res) {
   const { newTrainerName, trainerDescription } = req.body;
   const imagePath = "/images/trainers/" + req.file.filename;
-  await db.addNewTrainer(newTrainerName, trainerDescription, imagePath)
+  await db.addNewTrainer(newTrainerName, trainerDescription, imagePath);
   await trainerDefaultController(req, res);
 
   return;
 }
 
 async function deleteImage(image_path) {
-  const dependentTrainer = await db.trainerImagePathDependents(image_path)
-  if(dependentTrainer > 0) return;
+  const dependentTrainer = await db.trainerImagePathDependents(image_path);
+  if (dependentTrainer > 0) return;
 
-  const __filename = fileUrlToPath(import.meta.url);
+  const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(path.dirname(__filename));
-  const absolute_path = path.join(__dirname, "public", image_path )
-  await unlink(absolute_path)
+  const absolute_path = path.join(__dirname, "public", image_path);
+  await unlink(absolute_path);
 }
 
 export async function deleteTrainer(req, res) {
@@ -40,7 +46,7 @@ export async function deleteTrainer(req, res) {
 
   const image_path = rows[0].image_path;
 
-  await db.deleteTrainer(trainerID)
+  await db.deleteTrainer(trainerID);
   await deleteImage(image_path);
 
   res.sendStatus(204);
@@ -49,6 +55,10 @@ export async function deleteTrainer(req, res) {
 export async function updateTrainer(req, res) {
   const trainerID = req.params.id;
   const { updatedTrainerName, updatedTrainerDescription } = req.body;
-  await db.updateTrainer(trainerID, updatedTrainerName, updatedTrainerDescription)
+  await db.updateTrainer(
+    trainerID,
+    updatedTrainerName,
+    updatedTrainerDescription
+  );
   res.sendStatus(204);
 }
